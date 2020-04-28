688bfb5e3468f7b8b1c0383c82359cc6092f092f
KCN007 <www.kaniz98@gmail.com>
2020-04-29 00:43:09 +0600

    excercise 5

---
+++ 5_if_else_statement.php
@@ -0,0 +1,30 @@
+<html>
+<body>
+
+
+<script>
+	
+	function mim(value){
+		var c;
+	if(value == 22)
+	{
+		c="Today is 22th of the Month...";
+		
+	}
+	else
+	{
+		c="Today is not the 22th of the Month...";
+	}
+	return c;
+
+	}
+	
+	var value = 20;
+	d=mim(value);
+	document.write(d);
+
+</script>
+
+</body>
+</html>
+
