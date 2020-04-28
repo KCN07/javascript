1c5d115e6501b52b7d353572d68b1f8f0f1071d9
KCN007 <www.kaniz98@gmail.com>
2020-04-29 01:58:38 +0600

    excercise 7

---
+++ 7_get_element_by_id.php
@@ -0,0 +1,22 @@
+<html>
+<body>
+
+<h1>--- Example 2 ---</h1> 
+
+
+<script>
+	function myFunction()
+	{
+	  document.getElementById("change").innerHTML = "KANIZ FATIMA";  
+		
+		
+	}
+</script>
+	
+	<p id="change">kaniz fatima</p>
+	
+	<input type="submit" onClick="myFunction()" value="CLICK HERE" />
+
+
+</body>
+</html>
