e20dd6a6dd95c5ba2c7529a13f52e6a1cf7efc54
KCN007 <www.kaniz98@gmail.com>
2020-04-29 02:45:13 +0600

    excercise 10

---
+++ 10_get_element_by_tag_name.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<script>
+	function myFunction() 
+	{
+		
+		 document.getElementsByTagName("ul")[1]. getElementsByTagName("li")[0].innerHTML = "Computer Science and Engineering";
+		
+		
+	}
+</script>
+
+<ul>
+  <li>BBA</li>
+  <li>Business Administration</li>
+</ul>
+
+<ul>
+  <li>CSE</li>
+  <li>Computer Engineering</li>
+</ul>
+
+<button onclick="myFunction()">CHANGE</button>
+
+</body>
+</html>
