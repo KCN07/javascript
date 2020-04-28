3cdd16bf7f016e7f56e66ad33868e2f6d3d2aa58
KCN007 <www.kaniz98@gmail.com>
2020-04-29 00:34:06 +0600

    excercise 4

---
+++ 4_advanced_function.php
@@ -0,0 +1,20 @@
+<html>
+<body>
+
+<p>We will now call a function which will calculate two values and will return the result:</p>
+
+<script>
+function mim(i, j)
+{
+	k = i * j; 	
+				
+	return k;
+}
+
+
+document.write( mim(4, 3) );
+
+</script>
+
+</body>
+</html>
