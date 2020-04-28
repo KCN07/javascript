2122214a1a6b0635fc0163e77ede1bdb109ebf3e
KCN007 <www.kaniz98@gmail.com>
2020-04-29 00:30:26 +0600

    excercise 3

---
+++ 3_simple_function.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<script>
+
+function showName(n) 
+{
+	alert("Name is: " + n); 
+}
+</script>
+</head>
+
+<body>
+
+
+<input type="submit" onClick="showName('kaniz')" value="Click Here" /> 
+
+</body>
+</html>
