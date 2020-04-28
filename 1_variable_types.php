699d79858d1e3789cf78385c769a8aca05aa0fd5
KCN007 <www.kaniz98@gmail.com>
2020-04-29 00:12:07 +0600

    excercise 1

---
+++ 1_variable_types.php
@@ -0,0 +1,25 @@
+<script>
+     //string
+	 var kaniz = "Good Girl";
+	 document.write(kaniz +"<br>");
+	// NUMERIC VALUE
+	var x1 = 34.00;      // Written with decimals = Float
+	var x2 = 34;         // Written without decimals = Integer
+	document.write(x1+"<br>"+x2+"<br>");
+	// BOOLEAN VALUE
+	var x = true; // Boolean = True = 1
+	var y = false; // Boolean = False = 0
+    document.write(x+"<br>"+y+"<br>");
+	// NULL VALUE
+	var person = null; // Null = No Value // Null != 0;
+	document.write(person+"<br>");
+	// ARRAY VALUE
+	var cars = new Array; // Variable_Type Variable_Name = new Array();
+		cars[0] = "Toyota"; // First Element of Array
+		cars[1] = "Volvo"; // Second Element of Array
+		cars[2] = "BMW"; // Third Element of Array
+		
+	var carLength = cars.length;
+	document.write("Total Number of Car is: " + carLength+"<br>");
+
+</script>
