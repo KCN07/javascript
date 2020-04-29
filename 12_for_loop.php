94236e65810fa0a226d3bd0ca12d7c7d7cfa4aa0
KCN007 <www.kaniz98@gmail.com>
2020-04-29 23:23:45 +0600

    excercise 12

---
+++ 12_for_loop.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<script>
+	var cars = new Array();
+	cars[0] = "Toyota";
+	cars[1] = "Volvo";
+	cars[2] = "BMW";
+	cars[3] = "Tata";
+
+	var numberOfCar = cars.length; // 4
+	
+	for (i=0; i<numberOfCar ; i++)
+	{
+		document.write("<input type='text' name='firstname' />");
+	}
+	
+	
+</script>
+</br>
+</br>
+<input type = 'submit' name='submit' value ='submit' />
+</body>
+</html>
